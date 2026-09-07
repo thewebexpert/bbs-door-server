58aecd67a2f7a8cfb086a1714b6f695aee42a17f
Derek Bird <derek.bird@forces.gc.ca>
2026-09-07 19:31:45 -0400

    fix(openbox): symlink debian-menu.xml to suppress openbox startup warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,8 @@ cat << "EOF" > /root/.config/openbox/menu.xml
   </item>
 </menu>
 </openbox_menu>
-EOF
+mkdir -p /var/lib/openbox
+ln -sf /root/.config/openbox/menu.xml /var/lib/openbox/debian-menu.xml 2>/dev/null || true
 
 openbox &
 
